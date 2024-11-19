dd723e78d3228aee3fa957c40354c277a33a790b
nguyenkcr123 <1150080027@sv.hcmunre.edu.vn>
2024-11-20 00:17:56 +0700

    t đã thêm phần add

--- src/main/java/ecourse/controller/EnrollmentsController.java
+++ src/main/java/ecourse/controller/EnrollmentsController.java
@@ -4,8 +4,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
+import ecourse.model.Enrollments;
 import ecourse.model.EnrollmentsRepository;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
 
 
 
@@ -18,5 +25,16 @@ public class EnrollmentsController {
         model.addAttribute("list",enrRepository.findAll());
         return "admin/enrollments/index";
     }
+    //Thêm dữ liệu
+    @GetMapping("/admin/enrollments/add")
+    public String add() {
+        return "/admin/enrollments/add";
+    }
+    @PostMapping("/admin/enrollments/add")
+    public String add(@ModelAttribute Enrollments post) {
+        enrRepository.save(post);
+        return "redirect:/admin/enrollments";
+    }
+    
     
 }
