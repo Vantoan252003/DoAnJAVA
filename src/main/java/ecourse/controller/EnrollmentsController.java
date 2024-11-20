0706d988c32718d45ccbdae84c58096392a388c7
nguyenkcr123 <1150080027@sv.hcmunre.edu.vn>
2024-11-20 12:27:19 +0700

    ok

--- src/main/java/ecourse/controller/EnrollmentsController.java
+++ src/main/java/ecourse/controller/EnrollmentsController.java
@@ -6,16 +6,14 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import ecourse.model.CourseRepository;
 import ecourse.model.Enrollments;
 import ecourse.model.EnrollmentsRepository;
-import ecourse.model.UserClass;
 import ecourse.model.UserRepository;
 
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+
 
 
 
@@ -48,7 +46,7 @@ public class EnrollmentsController {
     @GetMapping("/admin/enrollments/edit/{enrollId}")
     public String edit(@PathVariable("enrollId") short enrollId, Model model) {
     Enrollments enroll = enrRepository.findById(enrollId).orElse(null);
-    model.addAttribute("enrollments", enroll);
+    model.addAttribute("enroll", enroll);
     return "admin/enrollments/edit";
     }
     @PostMapping("/admin/enrollments/edit/{enrollId}")
