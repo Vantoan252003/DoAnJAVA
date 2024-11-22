3bdb26782bb03927be718fce82260b0fd0d76524
nguyenkcr123 <1150080027@sv.hcmunre.edu.vn>
2024-11-22 16:48:43 +0700

    oke roi day

--- src/main/java/ecourse/controller/AssignmentsController.java
+++ src/main/java/ecourse/controller/AssignmentsController.java
@@ -1,14 +1,21 @@
 package ecourse.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import ecourse.model.Assignments;
 import ecourse.model.AssignmentsRepository;
+import ecourse.model.Enrollments;
+
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 
     @Controller
@@ -30,4 +37,24 @@ public class AssignmentsController {
         assignmentsRepository.save(assignments);
         return "redirect:/admin/assignments";
     }
+    //Sửa dữ liệu
+    @GetMapping("/admin/assignmets/edit/{assignmentId}")
+    public String edit(@PathVariable("assignmentId") short assignmentId, Model model) {
+    Assignments assignments = assignmentsRepository.findById(assignmentId).orElse(null);
+    model.addAttribute("assignmets", assignments);
+    return "admin/assignments/edit";
+    }
+    @PostMapping("/admin/assignments/edit/{assignmentId}")
+    public String update(@PathVariable("assignments") short assignmentId, @ModelAttribute Assignments assignments) {
+        assignments.setAssignmentId(assignmentId);
+        assignmentsRepository.save(assignments);
+        return "redirect:/admin/assignments";
+    }
+    //Xóa
+    @GetMapping("/admin/assignments/delete/{assignmentId}")
+    public String delete(@PathVariable("assignmentId") short assignmentId) {
+        assignmentsRepository.deleteById(assignmentId);
+        return "redirect:/admin/assignments";
+    }
+    
 }
