8b8a98ffbd1306895dea7f08b2c28425b72eb0b4
Vantoan252003 <1150080040@sv.hcmunre.edu.vn>
2024-11-23 11:23:22 +0100

    Sửa admin page

--- src/main/java/ecourse/controller/AdminController.java
+++ src/main/java/ecourse/controller/AdminController.java
@@ -5,8 +5,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class AdminController {
+
 @GetMapping("/admin")
-public String index() {
+public String trueAdmin() {
 return "admin/index";
 }
 }
--- src/main/java/ecourse/controller/LayoutController.java
+++ src/main/java/ecourse/controller/LayoutController.java
@@ -2,7 +2,7 @@ package ecourse.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 @Controller
